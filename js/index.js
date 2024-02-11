be518b4db4b2792ce17a8184508af21faf4bf3d1
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-02-11 15:01:29 +0000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
@@ -10,17 +9,4 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
-=======
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open')
-})
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
->>>>>>> 7ef4aa6b53ef55c6c214ec49bcf54997074f81fd
 })
